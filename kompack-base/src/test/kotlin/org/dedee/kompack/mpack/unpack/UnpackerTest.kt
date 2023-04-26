3bdf98d307b34a3886099f4a71c9dafbb83f5497
Dietrich Pfeifle <dd@dedee.de>
2023-04-26 09:56:19 +0200

    Added stream based sink for large docs

--- kompack-base/src/test/kotlin/org/dedee/kompack/mpack/unpack/UnpackerTest.kt
+++ kompack-base/src/test/kotlin/org/dedee/kompack/mpack/unpack/UnpackerTest.kt
@@ -1,7 +1,5 @@
 package org.dedee.kompack.mpack.unpack
 
-import org.dedee.kompack.mpack.pack.Packer
-import org.dedee.kompack.mpack.pack.SinkInMemory
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNull
 import org.junit.jupiter.api.Test
@@ -24,32 +22,13 @@ class UnpackerTest {
     }
 
     @Test
-    fun foo() {
-        val dest = ByteArray(100)
-        val s = Packer(SinkInMemory(dest))
-
-        val t = listOf(0, 5, -5, 0xff, -0xff, 0x7fff, 255, 256, 127, 128, -1, -2, Int.MIN_VALUE, Int.MAX_VALUE)
-
-        // Int.MAX_VALUE // 7FFFFFFF
-        // UInt.MAX_VALUE
-
-        t.forEach {
-            s.pack(it)
-        }
-
-//        val d = Unpacker(Source(s.build()))
-//        t.forEach {
-//            assertEquals(it, d.unpackInt())
-//        }
-    }
-
-    @Test
     fun reifiedTests() {
-//        val j:Int= Unpacker(Source(byteArrayOf(0x12.toByte()))).unpackk()
         val i: Int? = Unpacker(Source(byteArrayOf(0x12.toByte()))).unpakk()
         println(i)
+        assertEquals(0x12, i)
         val j: Long? = Unpacker(Source(byteArrayOf(0x12.toByte()))).unpakk()
         println(j)
+        assertEquals(0x12, j)
     }
 
 }
